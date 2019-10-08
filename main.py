492ff68e54200ca6d88c0bc8049a858a05ef9e5e
Andres Ladino <aladinoster@gmail.com>
2019-10-08 16:17:40 +0200

    :sparkles: Testing scenario for IDM
    including some lines of code to test the scenario on IDM model

--- main.py
+++ main.py
@@ -11,12 +11,21 @@ from bokeh.layouts import row, column
 output_notebook()
 
 #%%
-N = 50  # 50
+N = 100  # 50
 
 # Declaring Initial position and initial speed
 X0 = np.flip(np.arange(0, N) * (W_I + U_I) / (W_I * K_X))
 V0 = np.ones(N) * U_I
 A0 = np.zeros(N)
+
+# Tests IDM
+# DV0 = np.zeros(N)
+# S0 = np.zeros(N)
+# SD0 = np.zeros(N)
+# T10 = np.zeros(N)
+# T20 = np.zeros(N)
+# BS0 = np.zeros(N)
+
 
 veh_list = []
 
@@ -48,6 +57,15 @@ X = X0
 V = V0
 A = A0
 
+# Tests IDM
+# T1 = T10
+# T2 = T20
+# DV = DV0
+# S = S0
+# SD = SD0
+# BS = BS0
+
+
 for u in lead_acc:
     for veh in veh_list:
         veh.step_evolution(v_d=U_I, control=u)
@@ -56,11 +74,36 @@ for u in lead_acc:
     X = np.vstack((X, np.array([veh.x for veh in veh_list])))
     A = np.vstack((A, np.array([veh.a for veh in veh_list])))
 
+    # Tests IDM
+    # DV = np.vstack((DV, np.array([veh.dv for veh in veh_list])))
+    # S = np.vstack((S, np.array([veh.s for veh in veh_list])))
+    # SD = np.vstack((SD, np.array([veh.s_d() for veh in veh_list])))
+    # BS = np.vstack((BS, np.array([veh.break_strategy() for veh in veh_list])))
+    # T1 = np.vstack((T1, np.array([veh.t1() for veh in veh_list])))
+    # T2 = np.vstack((T2, np.array([veh.t2() for veh in veh_list])))
+
 ##%%
-pos = plot_trj(time, X, V)
-spd = plot_trj(time, V, V)
-acc = plot_trj(time, A, V)
-show(column(row(leader, pos), row(spd, acc)))
+pos = plot_trj(time, X, V, "Position")
+spd = plot_trj(time, V, V, "Speed")
+acc = plot_trj(time, A, V, "Acceleration")
+
+# Tests IDM
+# deltav = plot_trj(time, DV, V, "Delta V")
+# headway = plot_trj(time, S, V, "Headway")
+# dheadway = plot_trj(time, SD, V, "Desired headway")
+# breaking = plot_trj(time, BS, V, "Breaking")
+# t1 = plot_trj(time, T1, V, "Term 1")
+# t2 = plot_trj(time, T2, V, "Term 2")
+
+show(
+    column(
+        row(leader, pos),
+        row(spd, acc),
+        # row(deltav, breaking),
+        # row(headway, dheadway),
+        # row(t1, t2)
+    )
+)
 
 
 #%%
--- plotf.py
+++ plotf.py
@@ -1,20 +1,29 @@
 from bokeh.plotting import figure, show
 from bokeh.layouts import row
 from bokeh.io import output_notebook
-from bokeh.palettes import Viridis256
+from bokeh.palettes import Viridis256, Set1
+from bokeh.models import ColorBar, LinearColorMapper
 
 from carfollow import U_I
 
 
 def map_vc(v):
-    return Viridis256[int(min(((U_I - v) / U_I), 1) * 255)]
+    val = max(0, min(((U_I - v) / U_I), 1))  # Clamp values [0, 1]
+    return Viridis256[int(val * 255)]
 
 
-def plot_trj(time, data_x, data_v):
-    p = figure(title="Trajectories")
+def plot_trj(time, data_x, data_v, title="Curve vs time"):
+    p = figure(title=title, plot_height=500, plot_width=500)
+    v = range(0, 25)
+    # color_mapper = LinearColorMapper(
+    #     palette="Viridis256", high_color=Viridis256[255], low_color=Viridis256[255]
+    # )
+    # color_bar = ColorBar(color_mapper=color_mapper, border_line_color=None, location=(0, 0))
     for i, data in enumerate(zip(data_x.T, data_v.T)):
         x, v = data
         colors = [map_vc(c) for c in v[:-1]]
-        p.scatter(time, x[:-1], color=colors, radius=0.5)
+        p.scatter(time, x[:-1], color=colors, radius=0.3)
+        # p.line(time, x[:-1], color=Set1[9][i])
         p.line(time, x[:-1], color="gainsboro")
+    # p.add_layout(color_bar, "right")
     return p
