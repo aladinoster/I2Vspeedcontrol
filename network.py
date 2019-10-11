d738cfcd78d526e2de79d4c66a991808f9e93ddf
Andres Ladino <aladinoster@gmail.com>
2019-10-11 15:15:40 +0200

    :construction: Checklist Scoop
    WIP Traffic network

--- network.py
+++ network.py
@@ -1,3 +1,97 @@
 """
-    Network V2V / V2I 
+    Network Traffic + V2V 
 """
+
+# ===============================================================================
+# Imports
+# ===============================================================================
+
+from itertools import count
+from collections import deque
+
+import networkx as nx
+
+
+# ===============================================================================
+# Constants
+# ===============================================================================
+L_MAX = 20000
+
+# ===============================================================================
+# Classes
+# ===============================================================================
+
+
+class TrafficLane:
+
+    __idx = count(0)  # Vehicle ID
+
+    __slots__ = ["length", "veh_list", "idx"]
+
+    def __init__(self, length: float = L_MAX) -> None:
+        self.idx = next(self.__class__.__idx)
+        self.length = length
+        self.veh_list = deque([])
+
+    def attach_vehicle(self, vehicle) -> None:
+        """ Attach a vehicle a vehicle to a lane"""
+        self.veh_list.append(vehicle)
+        vehicle.veh_leader.set_leader(self.veh_list[-1])
+
+    def detach_vehicle(self) -> None:
+        """ Detach head vehicle a vehicle from the  lane"""
+        self.veh_list[1].control = self.veh_list[0].control
+        self.veh_list.pop()
+
+
+class TrafficLink:
+
+    __slots__ = ["_lanes", "idx"]
+    __idx = count(0)  # Vehicle ID
+
+    def __init__(self, length: float = L_MAX, n_lanes: int = 1) -> None:
+        self.idx = next(self.__class__.__idx)
+        self._lanes = tuple(TrafficLane(length) for n in range(n_lanes))
+
+
+class TrafficNetwork:
+
+    __slots__ = ["_links", "_lro", "idx"]
+    __idx = count(0)  # Vehicle ID
+
+    def __init__(self, lengthslinks: list = [L_MAX], laneslinks: list = [1]) -> None:
+        self.idx = next(self.__class__.__idx)
+        self._links = tuple(
+            TrafficLink(length, lanes) for length, lanes in zip(lengthslinks, laneslinks)
+        )
+        self._lro = set(lk.idx for lk in self._links)
+
+    def set_physical_connection(self, matrix_linkid):
+        pass
+
+    @property
+    def link_order(self):
+        """ Link resolution order"""
+        return self._lro
+
+    @link_order.setter
+    def link_order(self, lro):
+        """ lro stands for link resolution order"""
+        self._lro = lro
+
+    def __iter__(self):
+        """Iter protocol"""
+        return iter(self.link_order)
+
+    def __next__(self):
+        yield next(self.link_order)
+
+
+class ScenarioCase(TrafficNetwork):
+    """ Fills specific for a particular traffic network"""
+
+    def __init__(self):
+        pass
+
+
+
