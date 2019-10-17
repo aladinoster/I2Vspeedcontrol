f81b490a6d7cb1df803536b6bd02f051f08d182f
Andres Ladino <aladinoster@gmail.com>
2019-10-17 20:01:09 +0200

    ♻️ Cleaning some old code

--- support.py
+++ support.py
@@ -1,18 +1,11 @@
 """ 
     Support functionalities 
 """
-#%%
+
 import numpy as np
-from carfollow import SIGMA_A, U_I
+from carfollow import U_I
 
 np.random.seed(100)
-
-
-def gaussian(time, s=1):
-    """ Gaussian curve """
-    x = (time) / (s / 3)
-    # A = 1 / (np.sqrt(2 * np.pi) * s)
-    return np.exp(-x ** 2)
 
 
 def sigmoid(x, A: float = 1, a: float = 50, d: int = 250):
@@ -53,28 +46,12 @@ def acceleration_pulse(x, v0=U_I, drop: float = 1, delay: int = 250, duration: f
     return -deriv_pulse_sigmoid(x=x, A=drop, d=delay, duration=duration)
 
 
-def speed_drop(x,v0=U_I,drop:float=1,delay:int=250):
+def speed_drop(x, v0=U_I, drop: float = 1, delay: int = 250):
     """ Create a decreasing jump on speed"""
-    return v0-sigmoid(x,A=drop,d=delay)
+    return v0 - sigmoid(x, A=drop, d=delay)
 
 
 def shifter(signal, shift: int = 0):
     """ Circular shift of a signal """
     shift = np.clip(shift, 0, 480)  # Safe shift
     return np.roll(signal, shift)
-
-
-def leader_congestion_pattern(time, shift: int = 0):
-    """ Create congestion pattern """
-    lead_acc = -np.sin(2 * np.pi * 1 / 60 * (time)) * np.concatenate(
-        (np.ones(30), np.zeros(60), np.ones(30), np.zeros(len(time) - 120))
-    )
-    lead_acc += np.random.normal(0, 0.05, len(time))
-    return shifter(lead_acc, shift)
-
-
-def speed_change(time, change: float = 1, expander: float = 10, shift: int = 0):
-    """ Define control vehicle"""
-    vit_control = change * (-1 + gaussian(time, s=expander))
-    return shifter(vit_control, shift)
-
